35a7bdd9f9e5a5bb09ce30ec60a31cbde31ec1d7
wn <csignore@whril.com>
2025-03-19 12:17:38 -0400

    171

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -24,8 +24,8 @@ export default function Index() {
       <div className="inner-content">
       <div className="text">
       <p>To the Thinkers and Doers.</p>
-      <p>hey! I'm <u>Chris Signore</u> with the Progressive Conservative Party of Canada.</p>
-      <p>Today Canada is the greatest country in the world, but under Liberal leadership you'd never know it. Well folks, that stops now. My newsletter called The Citizen breaks down the political business of Parliament Hill and brings together the thinkers and doers of Canada who are doing great, great things.</p>
+      <p>Canada is the greatest country in the world, but under Liberal leadership, you'd never know it. Well folks, that stops now.</p>
+      <p>I'm Chris Signore with the Progressive Conservative Party of Canada, and my new newsletter The Citizen breaks down the political business of Parliament Hill and brings together the thinkers and doers of Canada who are doing great, great things.</p>
       <p>Are you a proud Canadian Citizen like me? Join my free newsletter, we're already 12,000 strong.</p>
       </div>
       <form method="post" action="https://app.jeffamzn.com/subscription/form">
