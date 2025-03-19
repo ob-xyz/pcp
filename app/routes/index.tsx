d4059ef01056fc334dbb4e93057f43fe9d88870a
wn <csignore@whril.com>
2025-03-19 14:36:23 -0400

    1122

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -24,7 +24,7 @@ export default function Index() {
       <div className="inner-content">
       <div className="text">
       <p>Stop the Psyop.</p>
-      <p>Canada has the greatest citizens in the world, but under Liberal leadership, you'd never know it. And that's why me and you are gonna stop the psyop, and clean up the slop.</p>
+      <p>Canada has the greatest citizens in the world, but under Liberal leadership, you'd never know it. That's why me and you are gonna stop the psyop, and clean up the slop.</p>
       <p>I'm <u>Chris Signore</u> with the Progressive Conservative Party of Canada, and my new newsletter <u>The Citizen</u> reports on the political business of Parliament Hill, and brings together proud Canadians just trying to avoid government psyop slop.</p>
       <p>Join me, and over 12,000 high agency Canadians who are already beginning to avoid the psyop slop.</p>
       </div>
